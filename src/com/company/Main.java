2f95cb5340efd511b14df842be1216bc0b5c1a65
HoangNhat <nhatlx159@gmail.com>
2020-12-29 19:08:04 +0700

    03

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hi World!!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap a:");
+        int a = sc.nextInt();
+        System.out.println("Nhap b:");
+        int b = sc.nextInt();
+        System.out.println("Nhap c:");
+        int c = sc.nextInt();
+        int k = b*b - 4*a*c ;
+        System.out.println("Delta = " + k);
+        }
 
     }
-
-}
