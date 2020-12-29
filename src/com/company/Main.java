f97ce6a5e99b39bc03090c4ce9b1d607ad399ddc
HoangNhat <nhatlx159@gmail.com>
2020-12-29 21:07:31 +0700

    02

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,17 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Nhap a:");
-        int a = sc.nextInt();
-        System.out.println("Nhap b:");
-        int b = sc.nextInt();
-        System.out.println("Nhap c:");
-        int c = sc.nextInt();
-        int k = b*b - 4*a*c ;
-        System.out.println("Delta = " + k);
-        }
-
+        String name1 = sc.next();
+        System.out.println("\t+ Nhap ho ten:" + name1);
+        int sn1 = sc.nextByte();
+        System.out.println("\t+ Nam sinh:" + sn1);
+        String gt1 = sc.next();
+        System.out.println("\t+ Gioi tinh:" + gt1);
+        float cc1 = sc.nextFloat();
+        System.out.println("\tChieu cao:" + cc1);
+        String cn1 = sc.next();
+        System.out.println("\tCan nang");
     }
+}
